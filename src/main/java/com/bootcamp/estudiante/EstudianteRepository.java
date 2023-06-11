98c6ad7690b566ea84af1b0d82680a425e081f56
miguel.cardenas <miguel.cardenas@ebcont.com>
2023-06-11 12:09:40 +0200

    native query done

--- src/main/java/com/bootcamp/estudiante/EstudianteRepository.java
+++ src/main/java/com/bootcamp/estudiante/EstudianteRepository.java
@@ -2,12 +2,13 @@ package com.bootcamp.estudiante;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface EstudianteRepository extends JpaRepository<Estudiante,Long> {
+public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
 
     boolean existsByEmailAndIdIsNot(String email, Long id);
 
@@ -17,6 +18,13 @@ public interface EstudianteRepository extends JpaRepository<Estudiante,Long> {
 //    List<Estudiante> findEstudianteByPrimerNombreOrPrimerApellido(String primerNombre, String primerApellido);
 
     // consulta con JPQL
-    @Query("select e from Estudiante e where e.primerNombre = ?1 or e.primerApellido = ?2")
-    List<Estudiante> findEstudianteByPrimerNombreOrPrimerApellido(String primerNombre, String primerApellido);
+//    @Query("select e from Estudiante e where e.primerNombre = ?1 or e.primerApellido = ?2")
+//    List<Estudiante> findEstudianteByPrimerNombreOrPrimerApellido(String primerNombre, String primerApellido);
+
+    // consulta con SQL
+    @Query(value = "SELECT * FROM estudiante WHERE primer_nombre = :primerNombre OR primer_apellido = :primerApellido", nativeQuery = true)
+    List<Estudiante> findEstudianteByPrimerNombreOrPrimerApellido(
+            @Param("primerNombre") String primerNombre,
+            @Param("primerApellido") String primerApellido
+    );
 }
