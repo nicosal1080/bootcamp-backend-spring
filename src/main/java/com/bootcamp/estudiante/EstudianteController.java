818ec7bcb3b57c58e46d3e0347ce5dc0e2362b61
miguel.cardenas <miguel.cardenas@ebcont.com>
2023-06-20 00:36:39 +0200

    sorting and pagination

--- src/main/java/com/bootcamp/estudiante/EstudianteController.java
+++ src/main/java/com/bootcamp/estudiante/EstudianteController.java
@@ -3,6 +3,9 @@ package com.bootcamp.estudiante;
 import com.bootcamp.libro.Libro;
 import com.bootcamp.materia.Materia;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,6 +31,15 @@ public class EstudianteController {
         }
 
         return estudianteService.getAllEstudiantes();
+    }
+
+    // este metodo podria remplazar el que nos retribuye la lista de estudiantes
+    @GetMapping("/paged")
+    public Page<Estudiante> getEstudiantes(@PageableDefault(size = 3, page = 0) Pageable pageable) {
+        // size = tamano de la pagina
+        // page = numero de pagina
+        // sort = orden sobre alguno de los atributos, podemos agregar direccion "asc", "desc"
+        return estudianteService.findAllEstudiantes(pageable);
     }
 
     @GetMapping("{id}")
--- src/main/java/com/bootcamp/estudiante/EstudianteService.java
+++ src/main/java/com/bootcamp/estudiante/EstudianteService.java
@@ -4,6 +4,8 @@ import com.bootcamp.libro.Libro;
 import com.bootcamp.libro.LibroRepository;
 import com.bootcamp.materia.Materia;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,6 +32,11 @@ public class EstudianteService {
         List<Estudiante> estudiantes = estudianteRepository.findAll();
 
         return estudiantes;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Estudiante> findAllEstudiantes(Pageable pageable) {
+        return estudianteRepository.findAll(pageable);
     }
 
     @Transactional(readOnly = true)
